fbd88a3899c03868f83d0644b11f947bb2ca2e46
LeVirus <charbonneau_cyril@hotmail.fr>
2016-05-09 18:32:40 +0200

    integration du menu de baudson

--- Code/androidDamePic/app/src/main/java/ceri/androiddamepic/MainActivity.java
+++ Code/androidDamePic/app/src/main/java/ceri/androiddamepic/MainActivity.java
@@ -1,6 +1,6 @@
 package ceri.androiddamepic;
 
-import android.content.Intent;
+/*import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,5 +21,47 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+}*/
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+
+            case R.id.nouvelle_partie:  // Lancer une nouvelle partie
+
+                Toast.makeText(getApplicationContext(), "Lancement de la partie !", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, TapisJeu.class));
+
+                break;
+            case R.id.parametre:        // Régler les paramètres
+                startActivity(new Intent(MainActivity.this, TapisJeu.class));
+                //setContentView(R.layout.main_menu);
+                break;
+
+            case R.id.exit:             // Quitter l'application
+                finish();
+                break;
+        }
     }
 }

d982a501f6737aa71bf1119bc8f9a56e21bbd6e2
LeVirus <charbonneau_cyril@hotmail.fr>
2016-05-10 15:54:04 +0200

    Ajout de UIPlayer pointeur null a regler

---
+++ Code/androidDamePic/app/src/main/java/Player/UIPlayer.java
@@ -0,0 +1,93 @@
+package Player;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import Game.Carte;
+import Game.IPlateau;
+import Game.IPlayer;
+import ceri.androiddamepic.TapisJeu;
+//import mad.damedepique.GameActivity;
+
+/*  Classe s'ocupent de l'interaction entre l'IHM et la Partie
+
+ */
+public class UIPlayer extends abstractPlayer implements IPlayer
+{
+    protected Context context;
+    protected TapisJeu gameActivity;
+    protected Lock lock;
+
+    public UIPlayer(TapisJeu gameActivity)
+    {
+        points = 0;
+        this.gameActivity = gameActivity;
+        this.lock = new ReentrantLock();
+    }
+
+
+    @Override
+    public Carte playCard()
+    {
+        //On rafraichie le plateau de jeu
+        //gameActivity.refreshScreen();
+        Toast.makeText(context, "A votre tour.", Toast.LENGTH_LONG).show();
+        lock.lock();
+        int c = gameActivity.playCard(carteEnMain);
+        Carte r = carteEnMain.get(c);
+        carteEnMain.remove(c);
+
+        //gameActivity.setnbSelectableCard(1);
+        lock.lock();lock.unlock();
+        //TODO: retourner la carte chosie.
+        return r;
+       // return null;
+    }
+
+    @Override
+    public Carte[] exchangeCards()
+    {
+        Toast.makeText(context, "Choisisez 3 cartes à échanger.", Toast.LENGTH_LONG).show();
+        Lock lock = new ReentrantLock();
+        lock.lock();
+
+        Carte[] r = new Carte[3];
+        int t[] = gameActivity.exchangeCardsPlayer(carteEnMain);
+        for(int i=0;i<3;i++)
+        {
+            r[i]=carteEnMain.get(t[i]);
+        }
+        for(int i=2;i>=0;i--)
+        {
+            carteEnMain.remove(t[i]);
+        }
+
+        lock.lock();lock.unlock();
+        //TODO: retourner les cartes chosie.
+        return r;
+    }
+
+    /*@Override
+    public void setBoard(IPlateau p)
+    {
+        plateau = p;
+        gameActivity.setPlateau(p);
+    }*/
+
+    //Permet de débloquer le thread courant
+    public void unlockThread()
+    {
+        lock.unlock();
+    }
+
+    //Retourne la main du joueur
+    public ArrayList<Carte> getHand(){return carteEnMain;}
+
+    public void linkActivity(TapisJeu tapisAndroid){
+        gameActivity = tapisAndroid;
+    }
+}
